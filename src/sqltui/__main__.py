9971adc38335d9276facb8a38fbce9a24687bcc0
Jake edison Bersabe <jakeedison.bersabe@gcash.com>
2025-11-22 09:01:41 +0800

    feat: remove demo print

--- src/sqltui/__main__.py
+++ src/sqltui/__main__.py
@@ -1,5 +1,3 @@
-from rich import print
-from rich.panel import Panel
 from sqltui.main import SqlTUI
 
 
@@ -10,14 +8,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-    print(
-        Panel.fit(
-            "[b magenta]Hope you liked the demo![/]\n\n"
-            "Please consider sponsoring me if you get value from my work.\n\n"
-            "Even the price of a ☕ can brighten my day!\n\n"
-            "https://github.com/sponsors/willmcgugan\n\n"
-            "- Will McGugan",
-            border_style="red",
-            title="Consider sponsoring",
-        )
-    )
